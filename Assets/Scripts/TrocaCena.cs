fe8bf59a1bdf61bb2da1d68713925a4630a04e00
Gabriel Henrique Xavier Librande <gabriel_librande@hotmail.com>
2021-04-23 00:38:17 -0300

    Menu for games

---
+++ Assets/Scripts/TrocaCena.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class TrocaCena : MonoBehaviour
+{
+    public string NomeDaCena;
+    public void changeS()
+    {
+        SceneManager.LoadScene(NomeDaCena);
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
